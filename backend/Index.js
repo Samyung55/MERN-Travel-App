88d6c138a188429a5447a9cf52dc3292514f6af1
Sam Fred <samyung05@gmail.com>
2023-04-18 10:49:01 -0400

    db

--- backend/Index.js
+++ backend/Index.js
@@ -8,8 +8,23 @@ const roomRouter = require("./controls/room");
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL, () => console.log("Mongo connected!"));
-
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to DB');
+  });
+  
+  mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose connection error: ${err}`);
+  });
+  
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose connection disconnected');
+  });
+  
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
