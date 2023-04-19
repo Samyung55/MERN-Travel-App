5aada63165449a0ec03e9963b72c308604efab26
Sam Fred <samyung05@gmail.com>
2023-04-19 13:21:56 -0400

    src

---
+++ client/src/layout/about/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "./about.module.css";
+import img1 from "../../assets/img1"
+import { GiPalmTree } from "react-icons/gi"
+import { FaUmbrellaBeach } from "react-icons/fa"
+import { BiHappy } from "react-icons/bi"
+import { useState } from "react"
+import {useDispatch} from 'react-redux'
+import { useNavigate } from "react-router-dom"
+import { search } from "../../hooks/search"
+
+const About = () => {
+    const [type, setType] = useState("");
+    const [startDate, setStartDate] = useState('')
+    const [endDate, setEndDate] = useState('')
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const handleSearch = () => {
+        const startD
+    }
+}
